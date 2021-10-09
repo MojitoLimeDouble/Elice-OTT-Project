760c1a46cd0e589234b957637aa658086b079fbc
[2기 1반] 남은열 <shrckxhr@naver.com>
2021-10-09 20:09:02 +0900

    Style: Route 및 component 코드 작성 방식 변경

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { Route, Link } from 'react-router-dom';
-import ContentsDetail from './components/ContentsDetail';
-import Main from './components/Main';
-import Movie from './components/Movie';
-import MyPage from './components/MyPage';
-import NonSignIn from './components/NonSignIn';
-import PotatoBasket from './components/PotatoBasket';
-import Signin from './components/Signin';
-import Signup from './components/Signup';
-import TV from './components/TV';
-import Navigation from './components/Navigation';
-import Footer from './components/Footer';
-import MainPopularity from './containers/MainContainer';
-import PotatoBasketPopularity from './containers/PotatoBasketContainer';
-
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import ContentsDetail from "./components/ContentsDetail";
+import Main from "./components/Main";
+import Movie from "./components/Movie";
+import MyPage from "./components/MyPage";
+import NonSignIn from "./components/NonSignIn";
+import PotatoBasket from "./components/PotatoBasket";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import TV from "./components/TV";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import MainPopularity from "./containers/MainContainer";
+import PotatoBasketPopularity from "./containers/PotatoBasketContainer";
 
 const App = () => {
   return (
@@ -21,17 +20,35 @@ const App = () => {
       <div>
         <Navigation />
       </div>
-      <Route exact path="/" component={NonSignIn} />
-      <Route path="/signin" component={Signin} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/main" component={MainPopularity} />
+      <Route exact path="/">
+        <NonSignIn />
+      </Route>
+      <Route path="/signin">
+        <Signin />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
+      <Route path="/main">
+        <MainPopularity />
+      </Route>
       {/* <Route path="/main" component={Main} /> */}
-      <Route path="/contents-detail/:id?" component={ContentsDetail} />
-      <Route path="/movie" component={Movie} />
-      <Route path="/tv" component={TV} />
+      <Route path="/contents-detail/:id?">
+        <ContentsDetail />
+      </Route>
+      <Route path="/movie">
+        <Movie />
+      </Route>
+      <Route path="/tv">
+        <TV />
+      </Route>
       {/* <Route path="/potato-basket/:id?" component={PotatoBasket} /> */}
-      <Route path="/potato-basket/:id?" component={PotatoBasketPopularity} />
-      <Route path="/mypage" component={MyPage} />
+      <Route path="/potato-basket/:id?">
+        <PotatoBasketPopularity />
+      </Route>
+      <Route path="/mypage">
+        <MyPage />
+      </Route>
       <div>
         <Footer />
       </div>
