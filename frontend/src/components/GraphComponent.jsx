f1f33b3b636d01f2f267d620048124807cdc56c5
[2기 1반] 남은열 <shrckxhr@naver.com>
2021-10-13 02:07:01 +0900

    Feat: Main Page Movie 및 TV 흥행 예측 정보 그래프 초안 코드 구현

---
+++ frontend/src/components/GraphComponent.jsx
@@ -0,0 +1,68 @@
+import { ResponsivePie } from "@nivo/pie";
+
+const MyResponsivePie = ({ data }) => (
+  <ResponsivePie
+    data={data}
+    margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+    innerRadius={0.5}
+    cornerRadius={5}
+    activeOuterRadiusOffset={8}
+    colors={{ scheme: "paired" }}
+    borderWidth={1}
+    borderColor={{ from: "color", modifiers: [["darker", "0.2"]] }}
+    arcLinkLabelsTextColor="#333333"
+    arcLinkLabelsOffset={9}
+    arcLinkLabelsDiagonalLength={17}
+    arcLinkLabelsThickness={2}
+    arcLinkLabelsColor={{ from: "color" }}
+    arcLabelsSkipAngle={10}
+    arcLabelsTextColor={{ from: "color", modifiers: [["darker", "2"]] }}
+    defs={[
+      {
+        id: "dots",
+        type: "patternDots",
+        background: "inherit",
+        color: "rgba(255, 255, 255, 0.3)",
+        size: 4,
+        padding: 1,
+        stagger: true,
+      },
+      {
+        id: "lines",
+        type: "patternLines",
+        background: "inherit",
+        color: "rgba(255, 255, 255, 0.3)",
+        rotation: -45,
+        lineWidth: 6,
+        spacing: 10,
+      },
+    ]}
+    legends={[
+      {
+        anchor: "bottom",
+        direction: "row",
+        justify: false,
+        translateX: 0,
+        translateY: 56,
+        itemsSpacing: 0,
+        itemWidth: 70,
+        itemHeight: 18,
+        itemTextColor: "#999",
+        itemDirection: "left-to-right",
+        itemOpacity: 1,
+        symbolSize: 18,
+        symbolShape: "circle",
+        effects: [
+          {
+            on: "hover",
+            style: {
+              itemTextColor: "#000",
+            },
+          },
+        ],
+      },
+    ]}
+  />
+);
+
+export default MyResponsivePie;
